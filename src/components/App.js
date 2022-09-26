2acaa25cf178d9cb47a70a4045158241014d9fc7
Anderson Leonardo <andersonleonardo.sapinto@gmail.com>
2022-09-26 17:55:49 -0300

    enter

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,18 @@ import forca0 from "./assets/forca0.png"
 
 export default function App() {
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    
+    
+    
+    
+    
     return (
         <>
             <main>
                 <img src={forca0} alt="forca0"></img>
-                <button className="inicia-jogo"> Escolher a Palavra</button>
+                <div className="direita">
+                    <button className="inicia-jogo"> Escolher a Palavra</button>
+                </div>
             </main>
             <footer>
                 <ul>
@@ -16,7 +23,10 @@ export default function App() {
                         {alfabeto.map((a) => (<button className="letras">{a}</button>))}
                     </li>
                 </ul>
-
+                <div className="chute" >
+                    <input type="text" placeholder="Já descobriu a palavra?"></input>
+                    <button>Chutar</button>
+                </div>
             </footer>
         </>
     )
